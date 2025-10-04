80f0366e62ccfb89bc0fddcf98e45dde1197419c
Michael Weingartner <mweingar@gmail.com>
2025-10-03 22:03:44 -0400

    Initial Commit

---
+++ DiceDungeon/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DiceDungeon
+//
+//  Created by Michael Weingartner on 10/3/25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
